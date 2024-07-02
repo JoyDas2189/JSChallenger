6909ce352cd78efac9f8652a9fb78165b6d84f27
Joy Das <dasjoy365d@gmail.com>
2024-07-03 00:42:00 +0600

    first commit

---
+++ array.js
@@ -0,0 +1,94 @@
+// Write a function that takes an array (a) and a value (n) as argument. Return the nth element of 'a'
+
+// function myFunction (a, n) {
+//     return a[n-1];
+// }
+// console.log(myFunction([1,2,3,4,5],3));
+
+// Write a function that takes an array (a) as argument. Remove the first 3 elements of 'a'. Return the result
+// function
+// myFunction(a){
+//     return a.slice(3);
+// }
+// console.log(myFunction([1,2,3,4])
+// )
+
+// Write a function that takes an array (a) as argument. Extract the last 3 elements of 'a'. Return the resulting array
+
+// function myFunction (a) {
+//     return a.slice(-3);
+// }
+// console.log(myFunction([1,2,3,4]));
+
+
+// Write a function that takes an array (a) as argument. Extract the first 3 elements of a. Return the resulting array
+
+// function myFunction (a) {
+//     return a.slice(0, 3);
+// }
+// console.log(myFunction([1,2,3,4]));
+
+
+// Write a function that takes an array (a) and a number (n) as arguments. It should return the last n elements of a.
+
+// function myFunction (a, n) {
+//     return a.slice(-n);
+// }
+// console.log(myFunction([1, 2, 3, 4, 5], 2));
+
+
+// Write a function that takes an array (a) and a value (b) as argument. The function should remove all elements equal to 'b' from the array. Return the filtered array.
+
+// function myFunction (a, b) {
+//     return a.filter((num) => {
+//         number !==0;
+//     })
+// }
+    
+// console.log(myFunction([1,2,3], 2));
+
+// Write a function that takes an array (a) as argument. Return the number of elements in a.
+
+// function myFunction (a) {
+//     return a.length;
+// }
+// console.log(myFunction([1,2,2,4]));
+
+// Write a function that takes an array of numbers as argument. Return the number of negative values in the array.
+
+// function myFunction (a) {
+//     return a.filter((val) => val < 0).length;
+// }
+
+
+// console.log(myFunction([1,-2,2,-4]));
+
+// Write a function that takes an array of strings as argument. Sort the array elements alphabetically. Return the result.
+
+// function myFunction (arr) {
+//     return arr.sort()
+// }
+// console.log(myFunction(['b', 'c', 'd', 'a']));
+
+
+// Write a function that takes an array of numbers as argument. It should return an array with the numbers sorted in descending order.
+// function myFunction (arr) {
+//     return arr.sort((a, b) => b - a);
+// }
+// console.log(myFunction([1,3,2]));
+
+
+// Write a function that takes an array of numbers as argument. It should return the sum of the numbers.
+// function myFunction (a) {
+//     return ;
+// }
+// console.log(myFunction([10,100,40]));
+
+// Write a function that takes an array of numbers as argument. It should return the sum of the numbers.
+
+// function myFunction(a) {
+//     return a.reduce((num1, num2) => {
+//         return num1 + num2;
+//     })
+// }
+// console.log(myFunction([10,100,40]))
