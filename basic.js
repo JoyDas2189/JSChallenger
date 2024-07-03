a874102f13abcbc55933327b5a0febb464ab6010
Joy Das <dasjoy365d@gmail.com>
2024-07-04 00:18:45 +0600

    more added

--- basic.js
+++ basic.js
@@ -1,3 +1,18 @@
+// Write a function that takes an array (a) and a value (n) as argument. Return the nth element of 'a'
+
+// function myFunction(a, n){
+//     return a[n-1];
+// }
+// console.log(myFunction([1,2,3,4,5],3))
+
+// Write a function that takes an array (a) as argument. Remove the first 3 elements of 'a'. Return the result
+
+// function elementRemover () {
+
+// }
+// let value = [1, 2, 3, 4];
+// elementRemover(value);
+
 // Write a function that takes an array of numbers as argument. It should return the sum of the numbers.
 
 
@@ -46,14 +61,43 @@
 
 // Write a function that takes an array as argument. It should return true if all elements in the array are equal. It should return false otherwise.
 
-function equalityChecker(arr) {
-    for (let i = 0; i < arr.length; i++) {
-        let firstElement = arr[0];
-        if(arr[i] !== firstElement) {
-            return false;
+// function equalityChecker(arr) {
+//     for (let i = 0; i < arr.length; i++) {
+//         let firstElement = arr[0];
+//         if(arr[i] !== firstElement) {
+//             return false;
+//         }
+//     }
+//     return true;
+// }
+// let value = ['test', 'test', 'test'];
+// console.log(equalityChecker(value)); 
+
+
+// Write a function that takes an array of strings as argument. Return the longest string.
+
+/*
+const myFunction = (arr) => {
+    let longestValue = arr[0];
+    for(let i = 0; i < arr.length; i++) {
+        if(arr[i].length > longestValue.length) {
+            longestValue = arr[i];
         }
     }
-    return true;
+    return longestValue;
 }
-let value = ['test', 'test', 'test'];
-console.log(equalityChecker(value)); 
+console.log(myFunction(["Happy", "Birthday"]));
+*/
+
+const myFunction = (arr) => {
+    if(arr.length <= 3) {
+        return [];
+    }
+
+    let newArr = [];
+    for(let i = 3; i < arr.length; i++) {
+        newArr.push(arr[i]);
+    }
+    return newArr;
+}
+console.log(myFunction([1,2,3,4,5]));
